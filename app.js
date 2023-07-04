ab872067b068d26a3f08efb799c9f89d33fdf092
Igor Lemes <igorramosirl11@gmail.com>
2023-07-04 17:30:58 -0300

    Gerenciamento e carregamento de comandos adicionados

--- app.js
+++ app.js
@@ -1,14 +1,53 @@
-const { Client, Events, GatewayIntentBits } = require('discord.js');
+// Log in to Discord with your client's token
+const fs = require('node:fs');
+const path = require('node:path');
+const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
 const { token } = require('./config.json');
 
-// Create a new client instance
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
-// When the client is ready, run this code (only once)
-// We use 'c' for the event parameter to keep it separate from the already defined 'client'
-client.once(Events.ClientReady, c => {
-	console.log(`Ready! Logged in as ${c.user.tag}`);
+client.commands = new Collection();
+const foldersPath = path.join(__dirname, 'commands');
+const commandFolders = fs.readdirSync(foldersPath);
+
+for (const folder of commandFolders) {
+	const commandsPath = path.join(foldersPath, folder);
+	const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+	for (const file of commandFiles) {
+		const filePath = path.join(commandsPath, file);
+		const command = require(filePath);
+		if ('data' in command && 'execute' in command) {
+			client.commands.set(command.data.name, command);
+		} else {
+			console.log(`[AVISO] O comando em ${filePath} não possui as propriedades "data" ou "execute".`);
+		}
+	}
+}
+
+client.once(Events.ClientReady, () => {
+	console.log('Ready!');
 });
 
-// Log in to Discord with your client's token
+client.on(Events.InteractionCreate, async interaction => {
+	if (!interaction.isChatInputCommand()) return;
+
+	const command = interaction.client.commands.get(interaction.commandName);
+
+	if (!command) {
+		console.error(`Nenhum comando ${interaction.commandName} foi encontrado.`);
+		return;
+	}
+
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		if (interaction.replied || interaction.deferred) {
+			await interaction.followUp({ content: 'Houve um erro ao executar este comando!', ephemeral: true });
+		} else {
+			await interaction.reply({ content: 'Houve um erro ao executar este comando!', ephemeral: true });
+		}
+	}
+});
+
 client.login(token);
